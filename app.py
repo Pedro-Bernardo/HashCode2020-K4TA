55777809bafe8349d918e9010d8562a42f2a316f
Pedro Bernardo <pedro_bernardo@icloud.com>
2020-02-20 18:22:38 +0000

    libraries

--- app.py
+++ app.py
@@ -6,8 +6,26 @@ import sys
 
 
 def Main(input_path):
-    input_data = parseInput(input_path)
+    with open(input_path, 'r') as file:
+        lines = file.readlines()
+    
+    B = int(l[0])
+    L = int(l[1])
+    D = int(l[2])
+    scores = lines[3].split()
+
+    lines = lines[4:]
+
+    libs = []
+
+    for i in range(0, 2*L, 2):
+        lib = Library(line[i].split())
+        lib.add_ids(line[i+1].split())
+        libs.insert(lib, 0)
+
     
 
-if __name__ == '__main__':
+    
+
+if __name__ == '__main__': 
     Main(sys.argv[1])
--- library.py
+++ library.py
@@ -1,6 +1,26 @@
 class Library:
-    def __init__(self, books, time, books_day):
-        self.books = books
+    def __init__(self, parameters):
+        self.n_books = parameters[0]
         # time in days 0 -> D-1
-        self.time = time
-        self.books_p_day = books_day
+        self.time = parameters[1]
+        self.books_p_day = parameters[2]
+        self.ids = []
+        self.value = 0
+
+    def total_score(self, B):
+        total = 0
+        for i in ids:
+            total += B[i].get_score()
+        return total
+
+    def add_ids(self, ids):
+        self.ids = ids
+
+    def calc_value(self, B):
+        self.value = (total_score(B)/n_books)*books_p_day
+
+
+
+    # D - signup
+
+    
